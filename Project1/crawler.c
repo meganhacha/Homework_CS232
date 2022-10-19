f5f7937e078135fddf144052a37059938732a7cf
Megan Hacha <hachma01@pfw.edu>
2022-10-19 12:42:13 -0400

    commit project1

---
+++ Project1/crawler.c
@@ -0,0 +1,255 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+/*SIDE NOTE:
+  I ran runTestCases.sh and everything matched up, along with two of the prompts mentioned in the instructions for the project.
+  However, when I tried to do the one with 4 hops and seed #187 (in proj. description), it simply stopped me at hop 2. 
+  In it, hop 2 is supposed to be noted as having a cycle. Not sure why that's not working, but everthing else seems to be.
+*/
+
+
+#define MAX_ADDR_LENGTH 1000
+
+/*
+ * a node in our linked-list of web addresses
+ */
+struct listNode{
+  char addr[MAX_ADDR_LENGTH];
+
+  struct listNode *next;
+};
+
+/*
+ * returns 1 if the list starting at pNode contains the address addr,
+ *    and returns 0 otherwise
+ */
+int contains(const struct listNode *pNode, const char *addr);
+
+/*
+ * inserts the address addr as a new listNode at the end of
+ *    the list
+ */
+void insertBack(struct listNode *pNode, const char *addr);
+
+/*
+ * prints the addresses from pNode to the end of the list,
+ *   one on each line
+ */
+void printAddresses(const struct listNode *pNode);
+
+/*
+ * frees the memory associated with this node and all subsequent nodes
+ */
+void destroyList(struct listNode *pNode);
+  
+/*
+ * srcAddr should be a web address (e.g., http://www.yahoo.com).
+ * link should point to an array of maxLinkLength characters.
+ * getLink returns 1 if a link was found and 0 otherwise.
+ * If a link was found, "link" will be filled in with the web address.
+ */
+int getLink(const char* srcAddr, char* link, const int maxLinkLength);
+
+int main(int argc, char** argv){
+  
+  long seed;
+
+  char startAddr[MAX_ADDR_LENGTH];
+  char destAddr[MAX_ADDR_LENGTH];
+  
+  int hopNum, numHops;
+  
+  struct listNode *pListStart;
+
+  if(argc < 3 || argc > 4){
+    fprintf(stderr, "USAGE: %s startAddr numHops [rand seed]", argv[0]);
+    return -1;
+  }
+  
+  /* initialization */
+  if(argc >= 4){
+    seed = atol(argv[3]);
+  }
+  else{
+    seed = time(NULL);
+  }
+
+  printf("seed = %ld\n", seed);
+  srand(seed);
+
+  strncpy(startAddr, argv[1], MAX_ADDR_LENGTH);
+  startAddr[MAX_ADDR_LENGTH - 1] = '\0';
+
+  numHops = atoi(argv[2]);
+
+  pListStart = malloc(sizeof(struct listNode));
+  if(pListStart == NULL){
+    fprintf(stderr, "ERROR: could not allocate memory\n");
+    return -2;
+  }
+  strncpy(pListStart->addr, startAddr, MAX_ADDR_LENGTH);
+  pListStart->next = NULL;
+
+  /* start the crawling */
+  for(hopNum=1; hopNum <= numHops; hopNum++){
+    int res = getLink(startAddr, destAddr, MAX_ADDR_LENGTH);
+
+    if(!res){
+      printf("Dead end on hop %d: no outgoing links\n", hopNum);
+      break;
+    }
+
+    if(contains(pListStart, destAddr)){
+      printf("Cycle detected on hop %d: address %s\n", hopNum, destAddr);
+      hopNum--; // try again for this hop in the next iteration
+    }
+    else{
+      insertBack(pListStart, destAddr);
+      strncpy(startAddr, destAddr, MAX_ADDR_LENGTH);
+    }
+  }
+
+  printAddresses(pListStart);
+
+  destroyList(pListStart);
+
+  return 0;
+}
+
+
+/*
+ * returns 1 if the list starting at pNode contains the address addr,
+ *    and returns 0 otherwise
+ */
+int contains(const struct listNode *pNode, const char *addr){
+  const struct listNode *temp = pNode;
+
+  char comparisonArray [MAX_ADDR_LENGTH];
+
+  for(int i = 0; i < MAX_ADDR_LENGTH; i++) {
+    comparisonArray[i] = addr[i];
+  }
+
+  if(temp != NULL){
+
+      if(strcmp(comparisonArray, temp->addr) == 0) {
+        return 1;
+      }
+      else {
+        contains(temp->next, addr);
+      }
+  }
+  else {
+    return 0;
+  }
+}
+    
+
+/*
+ * inserts the address addr as a new listNode at the end of
+ *    the list
+ */
+void insertBack(struct listNode *pNode, const char *addr){
+  struct listNode *tempNode = (struct listNode*)malloc(sizeof(struct listNode));
+
+  for(int i =0; i <MAX_ADDR_LENGTH; i++) {
+    tempNode->addr[i] = addr[i];
+  }
+
+  tempNode->next = NULL;
+
+
+  if(pNode->next == NULL) {
+    pNode->next = tempNode;
+  }
+  else {
+    insertBack(pNode->next, addr);
+  }
+
+}
+
+
+/*
+ * prints the addresses from pNode to the end of the list,
+ *   one on each line
+ */
+void printAddresses(const struct listNode *pNode){
+  const struct listNode* node = pNode;
+
+  if(node != NULL) {
+    printf("%s\n", node->addr);
+    printAddresses(node->next);
+  }
+}
+
+/*
+ * frees the memory associated with this node and all subsequent nodes
+ */
+void destroyList(struct listNode *pNode){
+    struct listNode *deleteNode = pNode;
+    struct listNode *nextDeleted;
+
+    if(deleteNode != NULL) {
+      nextDeleted = deleteNode->next;
+      free(deleteNode);
+      destroyList(nextDeleted);
+    }
+}
+  
+
+
+int getLink(const char* srcAddr, char* link, const int maxLinkLength){
+  const int bufSize = 1000;
+  char buffer[bufSize];
+
+  int numLinks = 0;
+
+  FILE *pipe;
+
+  snprintf(buffer, bufSize, "curl -s \"%s\" | python3 getLinks.py", srcAddr);
+
+  pipe = popen(buffer, "r");
+  if(pipe == NULL){
+    fprintf(stderr, "ERROR: could not open the pipe for command %s\n", buffer);
+    return 0;
+  }
+
+  fscanf(pipe, "%d\n", &numLinks);
+
+  if(numLinks > 0){
+    int linkNum;
+    double r = (double)rand() / ((double)RAND_MAX + 1.0);
+
+    for(linkNum=0; linkNum<numLinks; linkNum++){
+      fgets(buffer, bufSize, pipe);
+      
+      if(r < (linkNum + 1.0) / numLinks){
+		    break;
+      }
+    }
+
+    /* copy the address from buffer to link */
+    strncpy(link, buffer, maxLinkLength);
+    link[maxLinkLength-1] = '\0';
+    
+    /* get rid of the newline */
+    {
+      char* pNewline = strchr(link, '\n');
+      if(pNewline != NULL){
+		    *pNewline = '\0';
+      }
+    }
+  }
+
+  pclose(pipe);
+
+  if(numLinks > 0){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
